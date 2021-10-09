99d5df50a1af6a10f7ca20235cef6dfe1841988a
Volkan <81369676+wolkann@users.noreply.github.com>
2021-10-09 11:27:17 +0300

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,62 @@
+import hashlib
+from typing import _SpecialForm 
+
+print("""
+ALGORİTHMS
+1- MD5
+2- SHA-1
+3- SHA-256
+""")
+
+choice = input("Algorithm Type: ")
+
+if (choice == "1"):
+
+    Diccionary = input("File Address: ")
+    hash1 = input("HASH: ")
+
+    listaslineas = open(Diccionary,"r").readlines()
+
+    for i in range(0,len(listaslineas)): 
+	    hash2 = hashlib.md5(listaslineas[i].replace("\n","").encode()).hexdigest()
+
+	    if hash1 == hash2:
+		    print("Password Found: "+listaslineas[i].replace("\n",""))
+		    exit()
+
+    print("HASH not found!")
+
+elif (choice == "2"):
+
+    Diccionary = input("File Address: ")
+    hash1 = input("HASH: ")
+
+    listaslineas = open(Diccionary,"r").readlines()
+
+    for i in range(0,len(listaslineas)): 
+	    hash2 = hashlib.sha1(listaslineas[i].replace("\n","").encode()).hexdigest()
+
+	    if hash1 == hash2:
+		    print("Password Found: "+listaslineas[i].replace("\n",""))
+		    exit()
+
+    print("HASH not found!")
+
+elif (choice == "3"):
+
+    Diccionary = input("File Address: ")
+    hash1 = input("HASH: ")
+
+    listaslineas = open(Diccionary,"r").readlines()
+
+    for i in range(0,len(listaslineas)): 
+	    hash2 = hashlib.sha256(listaslineas[i].replace("\n","").encode()).hexdigest()
+
+	    if hash1 == hash2:
+		    print("Password Found: "+listaslineas[i].replace("\n",""))
+		    exit()
+
+    print("HASH not found!")
+
+else:
+    print("Wrong keystroke!")
